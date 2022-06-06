7a9ab843e3e718ba893f672e0b61fd1b2d11397f
201250109 <201250109@smail.nju.edu.cn>
2022-06-06 21:11:02 +0800

    6月6日-21点10分
    制作了进行练习界面，但美化部分还需要稍微加强

--- pages/student/doHomework/doHomework.js
+++ pages/student/doHomework/doHomework.js
@@ -1,66 +1,20 @@
-// pages/student/doHomework/doHomework.js
+const app = getApp()
+
+import deviceUtil from "../../../miniprogram_npm/lin-ui/utils/device-util"
+
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+        capsuleBarHeight: deviceUtil.getNavigationBarHeight(),
+        exercises:[
+            { question: "1.小明家离学校有200米，那么请问小明的妈妈几岁？",
+              category: "singleObjective",
+              choices:[{choiceNumber:"A",choice:"赵"},{choiceNumber:"B",choice:"钱"},{choiceNumber:"C",choice:"孙"},        {choiceNumber:"D",choice:"李"}] },
+            { question: "2.以下哪些数字是质数？",
+              category: "multipleObjective",
+              choices:[{choiceNumber:"A",choice:"1"},{choiceNumber:"B",choice:"2"},{choiceNumber:"C",choice:"3"},        {choiceNumber:"D",choice:"4"}] },
+            { question: "3.小明家离学校有200米，写出小明学校的全称和小明的上学方式。",
+              category: "subjective",
+              answers:[{answer:"学校全程"},{answer:"上学方式"}] }
+        ]
     }
 })
