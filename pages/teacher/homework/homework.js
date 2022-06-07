e9b3c269421757f7ac8d363716855d83132492e5
201250109 <201250109@smail.nju.edu.cn>
2022-06-07 13:18:20 +0800

    6月7日-13点18分

--- pages/teacher/homework/homework.js
+++ pages/teacher/homework/homework.js
@@ -1,66 +1,21 @@
-// pages/teacher/homework/homework.js
+const app = getApp()
+
+import deviceUtil from "../../../miniprogram_npm/lin-ui/utils/device-util"
+
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        capsuleBarHeight: deviceUtil.getNavigationBarHeight(),
+        homeworks:[
+            {date:"5月27日",hwInThisDay:[{name:"作业1"},{name:"作业2"}]},
+            {date:"5月26日",hwInThisDay:[{name:"作业1"}]},
+            {date:"5月25日",hwInThisDay:[{name:"作业1"}]},
+            {date:"5月24日",hwInThisDay:[{name:"作业1"}]}
+        ]
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    
+    seeStatics(){
+        wx.navigateTo({
+            url: '/pages/teacher/HwStatics/HwStatics',
+        })
     }
 })
